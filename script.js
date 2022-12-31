938df92be509e00b80424aef1cfa0e0f4df798dd
Branles <97817501+Branles@users.noreply.github.com>
2022-12-31 19:18:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,21 @@ function animate() {
   const squareRect = square.getBoundingClientRect();
   if (squareRect.top < 0 || squareRect.bottom > window.innerHeight) {
     dy *= -1;
-    square.style.backgroundColor = getRandomColor();
+    changeShape();
   }
   if (squareRect.left < 0 || squareRect.right > window.innerWidth) {
     dx *= -1;
-    square.style.backgroundColor = getRandomColor();
+    changeShape();
   }
+}
+
+function changeShape() {
+  const shapes = ['square', 'circle', 'triangle'];
+  let currentShape = square.className;
+  let index = shapes.indexOf(currentShape);
+  index = (index + 1) % shapes.length;
+  square.className = shapes[index];
+  square.style.backgroundColor = getRandomColor();
 }
 
 function getRandomColor() {
@@ -30,14 +39,6 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
-
-function changeShape() {
-  const shapes = ['square', 'circle', 'triangle'];
-  let currentShape = square.className;
-  let index = shapes.indexOf(currentShape);
-  index = (index + 1) % shapes.length;
-  square.className = shapes[index];
 }
 
 document.addEventListener('click', changeShape);
