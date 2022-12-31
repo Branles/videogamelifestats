dd137e3a8904d358433ab35f6df43531d94675c1
Branles <97817501+Branles@users.noreply.github.com>
2022-12-31 22:43:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -33,28 +33,6 @@ function getRandomColor() {
   return color;
 }
 
-let startY;
-let endY;
-
-document.addEventListener('touchstart', function(event) {
-  startY = event.touches[0].clientY;
-});
-
-document.addEventListener('touchmove', function(event) {
-  endY = event.touches[0].clientY;
-});
-
-document.addEventListener('touchend', function() {
-  const swipeDistance = endY - startY;
-  if (swipeDistance > 0) {
-    // swipe down
-    scrollSpeed *= 0.9;
-  } else {
-    // swipe up
-    scrollSpeed *= 1.1;
-  }
-});
-
 document.addEventListener('click', function() {
   const scale = Math.random() + 0.5; // random value between 0.5 and 1.5
   square.style.transform = `scale(${scale})`;
@@ -63,7 +41,7 @@ document.addEventListener('click', function() {
 document.addEventListener('wheel', function(event) {
   if (event.deltaY > 0) {
     // scroll down
-    scrollSpeed *= 0.9;
+scrollSpeed *= 0.9;
   } else {
     // scroll up
     scrollSpeed *= 1.1;
