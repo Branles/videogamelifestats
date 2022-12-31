f466157a67ab19aef5afa0be12e7c410b9921c92
Branles <97817501+Branles@users.noreply.github.com>
2022-12-31 17:40:30 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const square = document.getElementById("square");
+const square = document.getElementById('square');
+let x = 0;
+let y = 0;
+let dx = 2;
+let dy = 2;
 
-setInterval(function() {
-  let x = Math.floor(Math.random() * window.innerWidth);
-  let y = Math.floor(Math.random() * window.innerHeight);
-  let color = '#'+Math.floor(Math.random()*16777215).toString(16);
-  square.style.top = x + "px";
-  square.style.left = y + "px";
-  square.style.backgroundColor = color;
-}, 100);
+function animate() {
+  requestAnimationFrame(animate);
+  x += dx;
+  y += dy;
+  square.style.top = y + 'px';
+  square.style.left = x + 'px';
+
+  // check for wall collisions
+  const squareRect = square.getBoundingClientRect();
+  if (squareRect.top < 0 || squareRect.bottom > window.innerHeight) {
+    dy *= -1;
+  }
+  if (squareRect.left < 0 || squareRect.right > window.innerWidth) {
+    dx *= -1;
+  }
+}
+
+animate();
