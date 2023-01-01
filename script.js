90864d2b01a65dccf9a402985b9bfddf89f7a6fb
Branles <97817501+Branles@users.noreply.github.com>
2023-01-02 09:04:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,64 +6,60 @@ let dy = 2;
 let scrollSpeed = 1;
 
 function animate() {
-requestAnimationFrame(animate);
-x += dx * scrollSpeed;
-y += dy * scrollSpeed;
-square.style.top = y + 'px';
-square.style.left = x + 'px';
+  requestAnimationFrame(animate);
+  x += dx * scrollSpeed;
+  y += dy * scrollSpeed;
+  square.style.top = y + 'px';
+  square.style.left = x + 'px';
 
-// check for wall collisions
-const squareRect = square.getBoundingClientRect();
-if (squareRect.top < 5) {
-dy *= -1;
-square.style.backgroundColor = getRandomColor();
-}
-if (squareRect.bottom > window.innerHeight - 5) {
-dy *= -1;
-square.style.backgroundColor = getRandomColor();
-}
-if (squareRect.left < 5) {
-dx *= -1;
-square.style.backgroundColor = getRandomColor();
-}
-if (squareRect.right > window.innerWidth - 5) {
-dx *= -1;
-square.style.backgroundColor = getRandomColor();
-}
+  // check for wall collisions
+  const squareRect = square.getBoundingClientRect();
+  if (squareRect.top < 5) {
+    dy *= -1;
+    square.style.backgroundColor = getRandomColor();
+  }
+  if (squareRect.bottom > window.innerHeight - 5) {
+    dy *= -1;
+    square.style.backgroundColor = getRandomColor();
+  }
+  if (squareRect.left < 5) {
+    dx *= -1;
+    square.style.backgroundColor = getRandomColor();
+  }
+  if (squareRect.right > window.innerWidth - 5) {
+    dx *= -1;
+    square.style.backgroundColor = getRandomColor();
+  }
 }
 
 function getRandomColor() {
-const letters = '0123456789ABCDEF';
-let color = '#';
-for (let i = 0; i < 6; i++) {
-color += letters[Math.floor(Math.random() * 16)];
-}
-return color;
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 square.addEventListener('click', function() {
-document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
 });
 
 document.body.addEventListener('click', function(event) {
-if (event.target === square) {
-return;
-}
-
-// Generate random size for square
-const size = Math.floor(Math.random() * 200) + 50; // random size from 50 to 250 pixels
-square.style.width = size + 'px';
-square.style.height = size + 'px';
+  // Generate random size for square
+  const size = Math.floor(Math.random() * 200) + 50; // random size from 50 to 250 pixels
+  square.style.width = size + 'px';
+  square.style.height = size + 'px';
 });
 
 document.addEventListener('wheel', function(event) {
-if (event.deltaY > 0) {
-// scroll down
-scrollSpeed *= 0.9;
-} else {
-// scroll up
-scrollSpeed *= 1.1;
-}
+  if (event.deltaY > 0) {
+    // scroll down
+    scrollSpeed *= 0.9;
+  } else {
+    // scroll up
+    scrollSpeed *= 1.1;
+  }
 });
 
 animate();
