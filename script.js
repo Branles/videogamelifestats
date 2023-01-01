0c98629473e956dbba0305091966dc38dd68828f
Branles <97817501+Branles@users.noreply.github.com>
2023-01-02 00:42:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -41,13 +41,9 @@ function getRandomColor() {
   return color;
 }
 
-document.addEventListener('click', function(event) {
-  if (event.button === 1) { // middle mouse button
-    document.body.style.backgroundColor = getRandomColor();
-  } else {
-    const scale = Math.random() + 0.5; // random value between 0.5 and 1.5
-    square.style.transform = `scale(${scale})`;
-  }
+document.addEventListener('click', function() {
+  const scale = Math.random() + 0.5; // random value between 0.5 and 1.5
+  square.style.transform = `scale(${scale})`;
 });
 
 document.addEventListener('wheel', function(event) {
@@ -58,6 +54,11 @@ document.addEventListener('wheel', function(event) {
     // scroll up
     scrollSpeed *= 1.1;
   }
+});
+
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault();
+  document.body.style.backgroundColor = getRandomColor();
 });
 
 animate();
