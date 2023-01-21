b82b27afa925e38543b8db42a17460bd274c30f5
Branles <97817501+Branles@users.noreply.github.com>
2023-01-21 18:05:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const rollDiceButton = document.getElementById("roll-dice");
 const diceResult = document.getElementById("dice-result");
 const moneyEarned = document.getElementById("money-earned");
+const playerName = document.getElementById("player-name");
+const playerMoney = document.getElementById("player-money");
+
+let totalMoney = 0;
 
 rollDiceButton.addEventListener("click", function() {
   const roll = Math.floor(Math.random() * 6) + 1;
   diceResult.innerHTML = `You rolled a ${roll}`;
+  totalMoney += roll;
   moneyEarned.innerHTML = `You earned ${roll} fictional money!`;
+  playerMoney.innerHTML = `Total money: ${totalMoney}`;
 });
