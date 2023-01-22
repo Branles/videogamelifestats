7ccdf7b2ca1df15d2bd14a5c507d7d9c588e42f9
Branles <97817501+Branles@users.noreply.github.com>
2023-01-22 11:48:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputMission = document.getElementById("input-mission");
 const submitMission = document.getElementById("submit-mission");
 const missionOutput = document.getElementById("mission-output");
+const completedMissions = document.getElementById("completed-missions");
 
 submitMission.addEventListener("click", function(e) {
   e.preventDefault();
@@ -8,5 +9,13 @@ submitMission.addEventListener("click", function(e) {
     missionOutput.innerHTML = "Please enter a mission";
   } else {
     missionOutput.innerHTML = `You have been sent on a mission to: ${inputMission.value}`;
+    // set a timeout to mark the mission as completed after 1 minute
+    setTimeout(function() {
+      missionOutput.innerHTML = "Mission completed!";
+      // create a new list item with the completed mission
+      const newMission = document.createElement("li");
+      newMission.innerHTML = inputMission.value;
+      completedMissions.appendChild(newMission);
+    }, 60000);
   }
 });
