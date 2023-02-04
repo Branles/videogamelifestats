3b790d9804e78685cdafd640e5ad54dec37342a9
Branles <97817501+Branles@users.noreply.github.com>
2023-02-04 21:39:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-let completedMissions = JSON.parse(localStorage.getItem("completedMissions")) || [];
-let timer;
-let missionInProgress = false;
+const deck = [];
+const waste = [];
+const foundations = [  [],
+  [],
+  [],
+  []
+];
+const tableau = [  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+];
 
-const inputMission = document.getElementById("input-mission");
-const submitMission = document.getElementById("submit-mission");
-const missionOutput = document.getElementById("mission-output");
-const completedMissionsList = document.getElementById("completed-missions-list");
-const timerOutput = document.getElementById("timer-output");
+// shuffle the deck of cards
+const shuffle = () => {
+  // code to shuffle the deck
+};
 
-submitMission.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (inputMission.value === "") {
-    missionOutput.innerHTML = "Please enter a mission";
-  } else if (missionInProgress) {
-    missionOutput.innerHTML = "A mission is already in progress. Please wait until it is completed.";
-  } else {
-    missionInProgress = true;
-    inputMission.setAttribute("disabled", "disabled");
-    missionOutput.innerHTML = `You have been sent on a mission to: ${inputMission.value}. It will be completed in 1 minute.`;
-    timer = 60;
-    timerOutput.innerHTML = timer;
-    const countdown = setInterval(() => {
-      timer--;
-      timerOutput.innerHTML = timer;
-      if (timer === 0) {
-        clearInterval(countdown);
-        missionInProgress = false;
-        completedMissions.push(inputMission.value);
-        localStorage.setItem("completedMissions", JSON.stringify(completedMissions));
-        missionOutput.innerHTML = `Mission: ${inputMission.value} has been completed and added to completed missions list`;
-        inputMission.removeAttribute("disabled");
-        inputMission.value = "";
-        completedMissionsList.innerHTML = "";
-        completedMissions.forEach(mission => {
-          const li = document.createElement("li");
-          li.innerHTML = mission;
-          completedMissionsList.appendChild(li);
-        });
-      }
-    }, 1000);
-  }
-});
+// initialize the game
+const init = () => {
+  shuffle();
+
+  // code to deal the cards to tableau and waste piles
+};
+
+// draw acard from the deck to the waste pile
+const draw = () => {
+// check if deck is not empty
+if (deck.length > 0) {
+const drawnCard = deck.pop();
+waste.push(drawnCard);
+}
+};
+
+// move a card from the waste pile to a foundation
+const moveToFoundation = (card, foundationIndex) => {
+// check if the move is valid
+if (/* check if the move is valid */) {
+const index = waste.indexOf(card);
+waste.splice(index, 1);
+foundations[foundationIndex].push(card);
+}
+};
+
+// move a card from a tableau pile to a foundation
+const moveToTableau = (card, tableauIndex, foundationIndex) => {
+// check if the move is valid
+if (/* check if the move is valid */) {
+const tableauPile = tableau[tableauIndex];
+const index = tableauPile.indexOf(card);
+tableauPile.splice(index, 1);
+foundations[foundationIndex].push(card);
+}
+};
+
+// move a card within a tableau pile or from a tableau pile to another
+const moveWithinTableau = (card, sourceIndex, targetIndex) => {
+// check if the move is valid
+if (/* check if the move is valid */) {
+const sourcePile = tableau[sourceIndex];
+const targetPile = tableau[targetIndex];
+const index = sourcePile.indexOf(card);
+targetPile.push(sourcePile.splice(index, 1)[0]);
+}
+};
+
+init();
