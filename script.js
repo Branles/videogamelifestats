84c9d0231bf3e3fd67623cf118c5e3ffde760f00
Branles <97817501+Branles@users.noreply.github.com>
2023-11-03 17:43:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-const square = document.getElementById("square");
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-const squareSize = 200; // Adjust the size of the square if needed
-
-let x = (screenWidth - squareSize) / 2;
-let y = (screenHeight - squareSize) / 2;
-let speedX = 2;
-let speedY = 2;
-
-function updateSquarePosition() {
-    x += speedX;
-    y += speedY;
-
-    if (x + squareSize > screenWidth || x < 0) {
-        speedX *= -1;
-    }
-
-    if (y + squareSize > screenHeight || y < 0) {
-        speedY *= -1;
-    }
-
-    square.style.left = x + "px";
-    square.style.top = y + "px";
-
-    requestAnimationFrame(updateSquarePosition);
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
-
-updateSquarePosition();
+</script>
